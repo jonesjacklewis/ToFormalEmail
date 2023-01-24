392232d698763ce987d66a6455e65efa8e27af16
jonesjacklewis <jones.jack.lewis@gmail.com>
2023-01-24 09:57:15 +0000

    Added discord integration

---
+++ discord-bot.py
@@ -0,0 +1,32 @@
+from main import get_token, get_prompt, generate_email
+import dotenv
+import os
+import discord
+
+dotenv.load_dotenv(".env")
+TOKEN = os.getenv("BOT_TOKEN")
+API_TOKEN = os.getenv("TOKEN")
+
+intents = discord.Intents.all()
+
+client = discord.Client(intents=intents)
+
+@client.event 
+async def on_ready():
+    print(f'Successful Launch!!! {client.user}') 
+    
+@client.event
+async def on_message(message):
+    print("Message received")
+    prompt: str = message.content
+
+    print(message)
+
+    if prompt.startswith("!formal"):
+        prompt = prompt.replace("!formal", "")
+        print(prompt)
+        response: str = generate_email(prompt, API_TOKEN)
+        print(response)
+        await message.channel.send(response)
+
+client.run(TOKEN)
